8a8f53a4333f38162d62c4ac1e3cdc92b51d9fd3
Blake Rose <bdecktor@gmail.com>
2019-06-16 15:18:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,15 @@ class Transfer
   end
 end
 
-def execute_transaction
-  if @sender.balance > @amount && @status == "pending"
-    @sender.balance -= @ amount
-    @receiver.balance += @amount
-    @status = "complete"
-  else
-    @status = "rejected"
-    return "Transaction rejected. Please check your account balance"
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @ amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance"
+    end
   end
+  
 end
