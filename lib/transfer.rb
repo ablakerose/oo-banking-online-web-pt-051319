016d4c2ae91f3958576c64b1e6c22cc6491750fb
Blake Rose <bdecktor@gmail.com>
2019-06-16 15:32:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-    
+
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
