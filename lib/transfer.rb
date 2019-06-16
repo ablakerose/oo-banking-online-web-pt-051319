bd9dc53220f4f1636bda16ba4f1f1a7376cf4c83
Blake Rose <bdecktor@gmail.com>
2019-06-16 15:02:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,9 @@ class Transfer
   attr_accessor :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
-    :sender, :receiver, :amount
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
 
   end
 
