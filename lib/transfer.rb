e144aee99c295c75e276bb21761cf95fdd0a57a2
Blake Rose <bdecktor@gmail.com>
2019-06-16 15:32:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
 
   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
-      @sender = @sender.balance - @amount
-      @receiver = @receiver.balance + @amount
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     
     else
